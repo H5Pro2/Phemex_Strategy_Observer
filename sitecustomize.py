ebca1983547299bd3a537641d29bba0e034cc577
Pascal.E <pascalernst79@gmail.com>
2026-05-17 11:10:07 +0200

    chore: auto-apply brain replay enhancements

--- sitecustomize.py
+++ sitecustomize.py
@@ -6,3 +6,10 @@ try:
     apply_agent_runtime_role_patch()
 except Exception:
     pass
+
+try:
+    from brain_replay_enhancements import apply_brain_replay_enhancement_patch
+
+    apply_brain_replay_enhancement_patch()
+except Exception:
+    pass
